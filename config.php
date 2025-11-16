33132ef3982fa4aa31165ae80125c60010590e4a
sourceid-max <sourceid@gmail.com>
2025-11-16 18:23:44 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
 // Configuration
 $botName = "CS Bot";
-$admin_password = 'admin123';
+$admin_password = 'admin123'; // change this on use on real server
 $data_dir = './';
 $chat_dir = 'chat/';
-$ElizaPart = 75;
-$threshold = 0.58; // set to 0.3 - 0.6
+$ElizaPart = 75; // in percentage
+$threshold = 0.58; // set to 0.3 - 0.6 for NLP best result
 
 ?>
